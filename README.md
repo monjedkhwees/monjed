f5a886683b4cba65d494e2199dabeb3fcd410222
monjedkhwees <monjedkhwees@gmail.com>
2016-08-20 22:22:59 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+#map neighborhood
+
+This project is a map of the neighborhood when running with a map showing signs of famous places and there is a list box to search for places
+Used in the application knockout , google map API
+___
+
+####Use Google Maps API
+
+ create a single page application using
+Google Maps API. The steps :
+
+Create a Google Project and obtain an API key for Google Maps
+Load the Google Maps API asynchronously
+Create an array of markers that would load when the application is launched
+ Create infowindows that will appear when a marker or its corresponding list
+   item is clicked
+
+---
+
+For this project I made several personalizations. I used `BOUNCE` to indicate
+when a marker or list item was clicked. I also adjusted the color of markers
+when a user drags the cursor over the markers on the map.
+
+___
+
+###Running the application
+
+A working internet connection is required to run the application. When
+`neighborhood-map.html` is launched, the map will load with all markers visible.
+The user can zoom and scroll on the map.
---
+++ js/map.js
@@ -0,0 +1,144 @@
+var map;
+var marker;
+var infowindow;
+var locations = [{
+    name: 'bairut',
+    lat: 33.893197,  
+    lng: 35.501933
+  },
+  {
+    name: "Alrawshe",
+    lat: 33.887212,
+    lng: 35.473351
+  },
+  {
+    name: 'sanae',
+    lat: 33.893421,  
+    lng: 35.490153 
+  },
+  {
+    name: 'amircan unevercity',
+    lat: 33.889787, 
+    lng: 35.474789
+  },
+  {
+    name: 'International College',
+    lat: 33.893421,  
+    lng: 35.490153
+  },
+  {
+    name: "barbar",
+    lat: 33.892744,
+    lng: 35.471570
+  }
+];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 33.893197,
+      lng: 35.501933
+    },
+    zoom: 13
+  });
+  infowindow = new google.maps.InfoWindow({
+    content: "<p>My Info Window Content</p>"
+  });
+  
+  var viewModel = function() {
+    var self = this;
+    self.locations = ko.observableArray(locations);
+    
+   self.locations().forEach(function(location) {
+      // create marker object for each location
+      var marker = new google.maps.Marker({
+        position: {
+          lat: location.lat,
+          lng: location.lng
+        },
+        map: map
+      });
+      // store reference to marker in marker property of location item
+      location.marker = marker;
+      // attach click event listener to marker
+      marker.addListener('click', function() {
+        //console.log("click");
+        infowindow.setContent('<p>' + location.name + '</p>');
+        infowindow.open(map, marker);
+        toggleBounce();
+      });
+    });
+
+
+function createClickEvent(location) {
+    location.marker.addListener('click', function() {
+        /* Start off by logging the location object to the console
+         * This will let you view the location object, and see if this `createClickEvent` function is working properly so far
+         */
+        console.log(location);
+
+        /*
+         * Inside this 'createClickEvent' function, the marker object can be accessed using 'location.marker'.
+         * You can use the marker variable to complete the necessary click functionality
+         */
+
+        /* TODO: use the 'setContent()' method to update the content of the 'infowindow' object */
+        toggleBounce(marker);
+        /* TODO: open the 'infowindow' object */
+        infowindow.open(map, marker);
+
+        /* TODO: bounce the map marker icon */
+        marker.addListener('click', toggleBounce);
+
+    });
+}
+
+
+
+function toggleBounce(marker) {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+
+/*function drop() {
+  for (var i =0; i < markerArray.length; i++) {
+    setTimeout(function() {
+      addMarkerMethod();
+    }, i * 200);
+  }
+}*/
+
+    self.value = ko.observable('');
+  self.search = ko.computed(function() {
+      return ko.utils.arrayFilter(self.locations(), function(place) {
+        console.log(place);
+        var match = place.name.toLowerCase().indexOf(self.value().toLowerCase()) >= 0;
+        place.marker.setVisible(match);
+        return match;
+      });
+    });
+    
+  };
+
+
+  ko.applyBindings(new viewModel());
+}
+
+  function createMarker(latlng) {
+  marker = new google.maps.Marker({
+    position: latlng,
+    map: map
+  });
+
+ return marker;
+} 
+
+function googleError(){
+  window.alert(" please try again");
+}
+
+
